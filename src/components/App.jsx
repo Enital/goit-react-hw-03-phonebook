1c545bca0861144dec20f443bc91c8a6faf49edb
Enital <117684114+Enital@users.noreply.github.com>
2023-06-01 15:18:21 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,36 +8,51 @@ import css from './app.module.css'
 class App extends React.Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
     filter: '',
   }
 
-  componentDidUpdate() {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  componentDidUpdate(PrevProps, PrevState) {
+    if (PrevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
   
+  componentDidMount() {
+    const dataFromLocalStorage = JSON.parse(localStorage.getItem('contacts'));
+    if (dataFromLocalStorage) {
+      this.setState({ contacts: dataFromLocalStorage });
+    }
+  }
+
   handleChange = event => {
     const { name, value } = event.currentTarget;
     this.setState({ [name]: value });
   };
 
   formSubmit = ({ id, name, number }) => {
-    const names = this.state.contacts.map(contact => contact.name);
+    if (this.state.contacts) {
+      const names = this.state.contacts.map(contact => contact.name);
     
-    const lowerCaseName = name.toLowerCase();
-    const lowerCaseNames = names.map(name => name.toLowerCase());
-    if (lowerCaseNames.includes(lowerCaseName)) {
-      alert(`${name} is already in Contacts!`);
-      return;
+      const lowerCaseName = name.toLowerCase();
+      const lowerCaseNames = names.map(name => name.toLowerCase());
+      if (lowerCaseNames.includes(lowerCaseName)) {
+        alert(`${name} is already in Contacts!`);
+        return;
+      }
+    
+      this.setState(prevState => {
+        return {
+          contacts: [{ id, name, number }, ...prevState.contacts],
+        };
+      })
     }
-    this.setState(prevState => {
-      return {
-        contacts: [{ id, name, number }, ...prevState.contacts],
-      };
-    })
+    else {
+      this.setState({ contacts: { id, name, number } })
+    }
   } 
 
   deleteContact = id => {
@@ -48,12 +63,14 @@ class App extends React.Component {
   };
 
   filterContacts = () => {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+    if (this.state.contacts) {
+      const { contacts, filter } = this.state;
+      const filteredContacts = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+      );
 
-    return filteredContacts;
+      return filteredContacts;
+    }
   };
 
   render() {
